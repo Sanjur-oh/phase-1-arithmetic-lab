11d84281bb201c35300dc4bb48c0fac80dd3cdd4
Michael Franco <franco.luis.michael@gmail.com>
2022-03-16 19:51:45 -0500

    commit tree 235b380f9de222bec4bc11e6f8e0b02466374555

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
 
-// Write your code here
+// Write01`` multiply (num1, num2) {
+let num1 = 2;
+let num2 = 31;
+const multiply = num1 * num2;
+
+const random = Math.floor(Math.random() *10 + 1)
+
+const num3 = 100;
+const num4 = 6;
+const mod = num3 % num4;
+
+const max = Math.max(1, 2, 5, 10, 20);
+
+
+//const Math.random();
+//const Math.floor(Math.random() * 10);
